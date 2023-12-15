8a023502a2f26097433f6e3bbf72048ee3e136a1
Serhii Kolesnyk <110739720+FlowMaster83@users.noreply.github.com>
2023-12-15 17:53:11 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Profile } from './Profile';
 import { Statistics } from './Statistics';
 
+
 import user from '../../src/data/user.json';
 import data from '../../src/data/data.json';
 
@@ -8,8 +9,7 @@ export const App = () => {
   return (
     <div>
       <Profile user={user} />
-      <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} />
+      <Statistics stats={data} title/>
     </div>
   );
 };
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,15 +1,1 @@
-import { Stats } from './Stats';
-import data from '../../src/data/data.json';
-
-export const Statistics = ({ label, percentage }) => {
-  return (
-    <ul>
-      {data.map(item => (
-        <li key={item.id}>
-          <Stats stats={label} />
-          <Stats stats={percentage} />
-        </li>
-      ))}
-    </ul>
-  );
-};
+export const Statistics = () => {};
